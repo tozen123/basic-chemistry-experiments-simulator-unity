db8e8240cf5e9b7d5b5633b978308f5b9ff2eddc
tozen123 <lanzuela.christian@gmail.com>
2022-12-07 19:25:11 +0800

    Update

---
+++ main_app/BASIC CHEMISTRY LAB SIMULATOR/Assets/Scripts/SimulatorMainManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulatorMainManager : MonoBehaviour
+{
+    public GameObject settingMenuCanvas;
+
+    private bool isOpened = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+}
